a8648c81ed28b34561c1f96c4117a744bf4c9a7a
Astik87 <astiksheriev@yandex.ru>
2023-10-21 03:20:22 +0300

    expo router

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
-module.exports = function(api) {
+process.env.EXPO_ROUTER_APP_ROOT = '../../src/pages';
+module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
+    plugins: [['module:react-native-dotenv'], ['expo-router/babel']],
     env: {
       production: {
         plugins: ['react-native-paper/babel'],
